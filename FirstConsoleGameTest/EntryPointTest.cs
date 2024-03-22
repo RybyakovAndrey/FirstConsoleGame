e3fecff015cb03224aa9afa17ebe5326640cec2d
Hard_Code <zero-0_0@bk.ru>
2024-03-23 00:23:50 +0500

    Create Unit test for ConsoleGeme Project

---
+++ FirstConsoleGameTest/EntryPointTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace FirstConsoleGameTest
+{
+    public class EntryPointTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [TestCase]
+        public void EntryPointTestInit()
+        {
+            Assert.Pass();
+        }
+    }
+}
